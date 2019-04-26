b3a1ddd11307dfb257b1c2b2b338a24da9bb0fcf
Chris Williams <cwill833@gmail.com>
2019-04-26 15:14:38 -0500

    board made

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-alert('loaded')
+/*----- constants -----*/ 
+
+
+/*----- app's state (variables) -----*/
+let board;
+let winner;
+let turn;
+
+/*----- cached element references -----*/ 
+
+
+
+/*----- event listeners -----*/ 
+
+
+
+/*----- functions -----*/
+function init(){
+    board = [
+        [0,0,0,0,0,0], // column 1
+        [0,0,0,0,0,0], // column 2
+        [0,0,0,0,0,0], // column 3
+        [0,0,0,0,0,0], // column 4
+        [0,0,0,0,0,0], // column 5
+        [0,0,0,0,0,0], // column 6
+        [0,0,0,0,0,0], // column 7
+    ];
+}
